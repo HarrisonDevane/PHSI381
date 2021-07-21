e35158c60624f10e82ef4f951fb9b90bc37de79b
HarrisonDevane <80191608+HarrisonDevane@users.noreply.github.com>
2021-07-21 16:26:53 +1200

    Delete Exercise_3.m

--- Exercise_3.m
+++
@@ -1,7 +0,0 @@
-temp = [-80, -60, -40, -20, 0, 20, 40, 60];
-row = [70, 85, 96, 110, 121, 134, 150, 163];
-
-plot(temp, row);
-title ('Temperature vs something');
-xlabel('Temperature (*C)');
-ylabel('Something (µΩ - cm)');
